ea5a030e71719fde8415a882b8b64bdefc2a20f6
Tiffany Hollenbeck <tjh820@yahoo.com>
2019-05-31 22:03:05 -0400

    bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var maxTriez = 10;
 var guessedLetterz = [];       
 var wordIndex;   
 var wins = []; 
+var losses = [];
 
 var user = [];           
 var remainingGuessez = 0;       
@@ -27,14 +28,41 @@ document.onkeydown = function(event) {
 
             userGuess(event.key.toLowerCase());
         }
-	}
-	
+    }
+    function displayword() {
+        document.getElementById("currentWord").innerHTML ="";
+        for (var i = 0; i < user.length; i++) {
+            document.getElementById("currentWord").innerHTML += user[i];
+        }
+        document.getElementById("remainingGuessez").innerHTML =remainingGuessez;
+       wins = document.getElementById("wins");
+		
+        document.getElementById("guessedLetterz").innerHTML = guessedLetterz;}
+        
+        function gameOver(){
+            wins:;
 
+        }
+        lettersMatched += guess;
+              if (numLettersMatched === currentWord.length) {
+                gameOver(true);}
+
+        function gameOver(wins) {
+            if (wins) {
+              output.innerHTML = messages.win;
+              output.classList.add('win');
+            } else {
+              output.innerHTML = messages.lose;
+              output.classList.add('error');
+            }
+        }        
+//resetting
 
 function reset() {
 
 		remainingGuessez =maxTriez;
-        
+        losses = 0
+        wins = 0
 		game = false;
 		
 			wordIndex = Math.floor(Math.random()*(wordBank.length));
@@ -49,25 +77,11 @@ function reset() {
        			 displayword();
 		};
 		
-
-
-function displayword() {
-        document.getElementById("currentWord").innerHTML ="";
-        for (var i = 0; i < user.length; i++) {
-            document.getElementById("currentWord").innerHTML += user[i];
-        }
-		document.getElementById("remainingGuessez").innerHTML =remainingGuessez;
-		
-		document.getElementById("guessedLetterz").innerHTML = guessedLetterz;
-		
-
-
-
         if(remainingGuessez <= 0) {
             
             hazFinished = true;
             
-        }
+        
 		};
 		
 
@@ -85,7 +99,18 @@ function userGuess(letter) {
             }
         } 
         displayword();
-		};
+        };
+
+        function gameOver(win) {
+            if (win) {
+              output.innerHTML = messages.win;
+              output.classList.add('win');}}
+
+             {
+                output.innerHTML = messages.lose;
+                output.classList.add('error');
+              };      
+    
 		
 
 
@@ -95,12 +120,16 @@ function rightGuess(letter) {
             if(wordBank[wordIndex][i] === letter) {
                 positions.push(i);
                
-			}
+            }
+            else {
+                lettersGuessed += guess;
+                losses--;}
 			
         }
         if (positions.length <= 0) {
             remainingGuessez--;
             wins++;
+            
         } else {
             for(var i = 0; i < positions.length; i++) {
                 user[positions[i]] = letter;
